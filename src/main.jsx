6c09f7421a32bfc15fac20b0d0aac390125a85f4
ContextlessIndustries <contextlessindustries@gmail.com>
2023-11-12 22:34:31 +0000

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SignIn from './SignIn.jsx'
+import SignUp from './SignUp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SignIn />
+  </React.StrictMode>,
+)
